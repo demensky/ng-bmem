b27d8a45699ad057cc2629cb3c861997abe6bf27
waterplea <alexander@inkin.ru>
2020-02-03 13:29:02 +0300

    initial setup

--- projects/ng-bmem/karma.conf.js
+++ projects/ng-bmem/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, '../../coverage/<%= dasherize(name) %>'),
+            dir: require('path').join(__dirname, '../../coverage/ng-bmem'),
             reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true,
         },
